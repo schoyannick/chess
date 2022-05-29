6b4af31a61999696d05f17885b4e30ed2046394d
Yannick Scho <scho.yannick@gmail.com>
2022-05-29 21:57:15 +0200

    Add queen and king movement

--- chess/King.h
+++ chess/King.h
@@ -4,11 +4,39 @@
 #include <vector>
 #include "Coordinate.h"
 
-class King : public Piece {
+class King : public Piece
+{
 	using Piece::Piece;
 
-	vector<Coordinate> getPossibleMoves() {
+	vector<Coordinate> getPossibleMoves(vector<Piece*> pieces) {
 		vector<Coordinate> coordinates = {};
+		vector<Coordinate> directs = {
+			{ -1, -1 },
+			{ 0, -1 },
+			{ 1, -1 },
+			{ 1, 0 },
+			{ 1, 1 },
+			{ 0, 1 },
+			{ -1, 0 },
+			{ -1, 1 }
+		};
+
+		for (auto& direct : directs) {
+			Coordinate newCoord = { direct.col + col, direct.row + row };
+
+			bool foundFriendly = false;
+
+			for (auto& piece : pieces) {
+				if (piece->col == newCoord.col && piece->row == newCoord.row && piece->color == color) {
+					foundFriendly = true;
+					break;
+				}
+			}
+
+			if (!foundFriendly) {
+				coordinates.push_back(newCoord);
+			}
+		}
 
 		return coordinates;
 	}
--- chess/Queen.h
+++ chess/Queen.h
@@ -4,11 +4,51 @@
 #include <vector>
 #include "Coordinate.h"
 
-class Queen : public Piece {
+class Queen : public Piece
+{
 	using Piece::Piece;
 
-	vector<Coordinate> getPossibleMoves() {
+	vector<Coordinate> getPossibleMoves(vector<Piece*> pieces) {
 		vector<Coordinate> coordinates = {};
+		vector<Coordinate> directs = {
+			{ -1, -1 },
+			{ 0, -1 },
+			{ 1, -1 },
+			{ 1, 0 },
+			{ 1, 1 },
+			{ 0, 1 },
+			{ -1, 0 },
+			{ -1, 1 }
+		};
+
+		for (auto& direct : directs) {
+			Coordinate newCoord = {};
+			newCoord.row = direct.row + row;
+			newCoord.col = direct.col + col;
+
+			while (newCoord.col >= 0 && newCoord.col < 8 && newCoord.row >= 0 && newCoord.row < 8) {
+				bool found = false;
+
+				for (auto& piece : pieces) {
+					if (piece->col == newCoord.col && piece->row == newCoord.row) {
+						found = true;
+						if (piece->color != color) {
+							coordinates.push_back(newCoord);
+						}
+						break;
+					}
+				}
+
+				if (found) {
+					break;
+				}
+
+				coordinates.push_back(newCoord);
+
+				newCoord.row += direct.row;
+				newCoord.col += direct.col;
+			}
+		}
 
 		return coordinates;
 	}
